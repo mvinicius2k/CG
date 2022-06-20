8968a122d0f8017bb6539a3b7fd36ab299a522b7
mvnicius2k <marcos.vinicius124816@gmail.com>
2022-06-20 14:34:24 -0300

    zpp

--- CG/utils/serializable.h
+++ CG/utils/serializable.h
@@ -1,14 +1,24 @@
 #pragma once
 #include <string>
-
+#include <vector>
 
 template <Serializable T>
 class Serializable
 {
 public:
 	virtual std::string serialize() = 0;
-	static T* Deserialize();
 	
+	static T* Deserialize(std::vector<std::string> &line)
+	{
+		auto obj = new T();
+		
+		
+	}
+
+	
+	
+protected:
+	virtual T* deserialize(vector<std::string>& line) = 0;
 
 private:
 
